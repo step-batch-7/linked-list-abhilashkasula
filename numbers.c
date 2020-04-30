460a98be594c0162f99aff2e6013df32ce7a87fe
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-04-30 15:57:10 +0530

    implemented add_to_start

--- numbers.c
+++ numbers.c
@@ -5,6 +5,8 @@ int main(void)
   List_ptr numbers = create_list();
   add_to_end(numbers, 5);
   add_to_end(numbers, 7);
+  add_to_start(numbers, 8);
+  add_to_start(numbers, 10);
   display(numbers);
   clear_list(numbers);
   destroy_list(numbers);
