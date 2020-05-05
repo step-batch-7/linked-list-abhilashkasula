a85b64dadde893f228814f6bdf8fd219ff1ca459
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-05-05 15:23:22 +0530

    renamed assert to assert_list in tests.c

--- test/tests.c
+++ test/tests.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "../list.h"
 
-unsigned int assert(List_ptr, int *, int);
+unsigned int assert_list(List_ptr, int *, int);
 
-unsigned int assert(List_ptr list, int *values, int length)
+unsigned int assert_list(List_ptr list, int *values, int length)
 {
   if (list->count != length)
     return 0;
@@ -23,21 +23,21 @@ void test_add_to_end(void)
 
   List_ptr list = create_list();
   int values[10] = {};
-  printf("\tNo values: %d\n", assert(list, values, 0));
+  printf("\tNo values: %d\n", assert_list(list, values, 0));
 
   add_to_end(list, 1);
   values[0] = 1;
-  printf("\tAdding to list when there are no elements in it: %d\n", assert(list, values, 1));
+  printf("\tAdding to list when there are no elements in it: %d\n", assert_list(list, values, 1));
 
   add_to_end(list, 2);
   values[1] = 2;
-  printf("\tAdding to list when there is an element in it: %d\n", assert(list, values, 2));
+  printf("\tAdding to list when there is an element in it: %d\n", assert_list(list, values, 2));
 
   add_to_end(list, 3);
   add_to_end(list, 4);
   values[2] = 3;
   values[3] = 4;
-  printf("\tAdding to list when there are more than two elements in it: %d\n", assert(list, values, 4));
+  printf("\tAdding to list when there are more than two elements in it: %d\n", assert_list(list, values, 4));
 }
 
 void test_add_to_start(void)
@@ -46,16 +46,16 @@ void test_add_to_start(void)
 
   List_ptr list = create_list();
   int values[10] = {};
-  printf("\tNo values: %d\n", assert(list, values, 0));
+  printf("\tNo values: %d\n", assert_list(list, values, 0));
 
   add_to_start(list, 1);
   values[0] = 1;
-  printf("\tAdding to list when there are no elements: %d\n", assert(list, values, 1));
+  printf("\tAdding to list when there are no elements: %d\n", assert_list(list, values, 1));
 
   add_to_start(list, 2);
   values[0] = 2;
   values[1] = 1;
-  printf("\tAdding to list when there is an element: %d\n", assert(list, values, 2));
+  printf("\tAdding to list when there is an element: %d\n", assert_list(list, values, 2));
 
   add_to_start(list, 3);
   add_to_start(list, 4);
@@ -63,7 +63,7 @@ void test_add_to_start(void)
   values[1] = 3;
   values[2] = 2;
   values[3] = 1;
-  printf("\tAdding to list when there are more than two elements: %d\n", assert(list, values, 4));
+  printf("\tAdding to list when there are more than two elements: %d\n", assert_list(list, values, 4));
 }
 
 int main(void)
